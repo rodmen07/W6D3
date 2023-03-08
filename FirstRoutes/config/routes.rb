ab08671a17adc870c90961f4b7aa7af5fb7f2eae
David Lai <pf.davidlai@gmail.com>
2023-03-08 15:09:49 -0800

    did routes

--- FirstRoutes/config/routes.rb
+++ FirstRoutes/config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users
+
+    # get 'users/:id', to: 'users#show', as: 'user'
+    # patch 'users/:id', to: 'users#update'
+    # put 'users/:id', to: 'users#update'
+    # delete 'users/:id', to: 'users#destroy'
+    # get 'users/:id/edit', to: 'users#edit', as: 'edit_user'
+    # get 'users/:new', to: 'users#new', as: 'new_user'
+    # post 'users/', to: 'users#create'
+    # get 'users/', to: 'users#index'
 
   # Defines the root path route ("/")
   # root "articles#index"
