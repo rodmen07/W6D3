47d0cac900a4bf08dd01f7ffbbbf7c9e56a4f61b
David Lai <pf.davidlai@gmail.com>
2023-03-08 17:30:59 -0800

    finished

--- FirstRoutes/app/controllers/users_controller.rb
+++ FirstRoutes/app/controllers/users_controller.rb
@@ -1,5 +1,17 @@
 class UsersController < ApplicationController
     def index
-        render plain: "I'm in the index action!"
+        
+        render json: User.all
+    end
+
+    def create
+        user = User.new(params.require(:user).permit(:name :email))
+  # replace the `user_attributes_here` with the actual attribute keys
+  user.save!
+  render json: user
+    end
+
+    def show
+        render json: params
     end
 end
