cd8dd5eba9140fc49ce97aeb73e2a4c58700cc03
Roderick Mendoza <rodmendoza07@gmail.com>
2023-03-09 09:09:33 -0800

    added changes to UserController

--- FirstRoutes/app/controllers/users_controller.rb
+++ FirstRoutes/app/controllers/users_controller.rb
@@ -1,17 +1,27 @@
 class UsersController < ApplicationController
+
     def index
-        
         render json: User.all
     end
 
     def create
-        user = User.new(params.require(:user).permit(:name :email))
-  # replace the `user_attributes_here` with the actual attribute keys
-  user.save!
-  render json: user
+        user = User.new(params.require(:user).permit(:name, :email))
+        # replace the `user_attributes_here` with the actual attribute keys
+        if user.save
+            render json: user
+        else
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end
     end
 
     def show
-        render json: params
+        render json: User.find(params[:id])
+    end
+
+    def update
+        render json: User.update(params[:id], )
+    end
+
+    def destroy
     end
 end
--- FirstRoutes/db/migrate/20230309004803_create_users.rb
+++ FirstRoutes/db/migrate/20230309004803_create_users.rb
@@ -3,7 +3,6 @@ class CreateUsers < ActiveRecord::Migration[7.0]
     create_table :users do |t|
       t.string :name, null:false
       t.string :email, null:false
-
       t.timestamps
     end
   end
